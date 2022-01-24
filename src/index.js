7af390646867bae62c41020f9827770cccaa5c7b
Alamicrodev <mohidalamsohail22@live.com>
2022-01-24 21:54:19 +0300

    started using css with react components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greetings() {
-   return (
-     <div>
-           <Name/> 
-           <Message/> 
-     </div>
-   )
-};
+// CSS
+import './index.css';
 
-
-const Name = () => {
-  return <h2> John Doe </h2>;
+function Booklist() {
+  return (
+    <section className="booklist">
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
+  )
 }
 
-const Message = () => {
-  return <p> This is my message </p>;
+const Book = () =>  {
+return (
+   <article className="book"> 
+     <Img></Img>
+      <Title/>
+      <Author/>
+   </article>
+)
 }
 
+const Img = () => { return (
+  <img src="https://images-na.ssl-images-amazon.com/images/I/91DNhLLmUOL._AC_UL200_SR200,200_.jpg" />
+)};
 
-ReactDom.render(<Greetings/>,document.getElementById("root"))
+
+const Title = () => { return ( <h1>Atlas Of The Heart</h1> ) };
+
+const Author = () => { return ( <h4 style={{color:'#3333', fontSize: '0.75rem', marginTop: "0.25rem"}} > This is an Author </h4> ) };
+
+const Description = () => { return ( <p> lorem20 </p> ) };
+
+
+ReactDom.render(<Booklist/>, document.getElementById("root"))
 
